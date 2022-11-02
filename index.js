407bd867d47dc2209fd62d1a2aeb2b3301819419
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2022-11-02 02:44:04 -0400

    Use sass middleware, add jsconfig and editorconfig

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const sassMiddleWare = require("node-sass-middleware");
 
 const app = express();
 const router = express.Router();
+
+app.use(
+  sassMiddleWare({
+    src: path.join(__dirname, "sass"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use('/', router);
-app.use(express.static(path.join(__dirname, 'css')))
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use(express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')))
+app.use("/", router);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use(
+  express.static(path.join(__dirname, "node_modules/@popperjs/core/dist/umd"))
+);
 
-app.listen(process.env.port || 3000, '0.0.0.0');
+port = process.env.port || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Listening on port ${port}`);
+});
